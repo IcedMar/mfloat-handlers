0d6b5cf21cef88a54436c4b6a29e3ce9402bcfd7
icedmar <mbanimarine@gmail.com>
2025-03-20 22:05:07 +0300

    Removed sensitive files from tracking

--- config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyAKNdNQrZRs1Fx7FQnTw3GABYbrqihcoMk",
-    authDomain: "the-m-float.firebaseapp.com",
-    projectId: "the-m-float",
-    storageBucket: "the-m-float.firebasestorage.app",
-    messagingSenderId: "91662213348",
-    appId: "1:91662213348:web:5777527c8eb4db851c4cf1"
-};
